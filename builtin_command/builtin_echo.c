67c036c97c11cbc4d6c26bd6bdc6215792b5d251
molhot <sakata19991214@gmail.com>
2023-03-17 12:03:36 +0900

    handling -n

--- builtin_command/builtin_echo.c
+++ builtin_command/builtin_echo.c
@@ -6,17 +6,18 @@
 /*   By: mochitteiunon? <sakata19991214@gmail.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 20:19:18 by user              #+#    #+#             */
-/*   Updated: 2023/03/17 11:54:18 by mochitteiun      ###   ########.fr       */
+/*   Updated: 2023/03/17 12:02:41 by mochitteiun      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void	echo_helper(char **commands, size_t position)
+static void	echo_helper(char **commands, size_t *position)
 {
-	write(1, commands[position], ft_strlen(commands[position]));
-	if (commands[position + 1] != NULL)
+	write(1, commands[*position], ft_strlen(commands[*position]));
+	if (commands[*position + 1] != NULL)
 		write(1, " ", ft_strlen(" "));
+	*position = *position + 1;
 }
 
 int	ms_echo(char *line, t_command *command)
@@ -34,18 +35,12 @@ int	ms_echo(char *line, t_command *command)
 		while (ft_strcmp(commands[position], "-n") == 0)
 			position++;
 		while (commands[position] != NULL)
-		{
-			echo_helper(commands, position);
-			position++;
-		}
+			echo_helper(commands, &position);
 	}
 	else
 	{
 		while (commands[position] != NULL)
-		{
-			echo_helper(commands, position);
-			position++;
-		}
+			echo_helper(commands, &position);
 		write(1, "\n", ft_strlen("\n"));
 	}
 	free_commands(commands);
